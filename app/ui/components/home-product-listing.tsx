a92e2df4bf4ec3b8518006d3f702e6757603fc7d
wcamer <xeus.xiii@hotmail.com>
2024-06-28 16:46:17 -0600

    Pagination and Product filter buttons components added

--- app/ui/components/home-product-listing.tsx
+++ app/ui/components/home-product-listing.tsx
@@ -1,6 +1,8 @@
 // 'use client'
 import '@/app/ui/global.css';
 import ProductDisplay from "./product-display"
+import { LeftArrow, RightArrow } from './arrows';
+import { ProductFilterButtons } from './product-filter';
 import clsx from 'clsx'
 import { useState } from 'react';
 
@@ -16,39 +18,67 @@ export default function ProductListing() {
         
         <div>
             <div>
-                <h2>Product Listing</h2>
+                <h2 className='text-center'>Product Listing</h2>
             </div>
+
             <div>
-            <article className="p-2 m-2 grid gap-4 grid-cols-2  md:grid-cols-3 lg:grid-cols-5">
-           {/* Placeholder products for now */}
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
-            <ProductDisplay />
+                <ProductFilterButtons />
+            </div>
+
+
+            
+
+            <div>
+                <article className="p-2 m-2 grid gap-4 grid-cols-2  md:grid-cols-3 lg:grid-cols-5">
+                    {/* Placeholder products for now */}
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        {/* <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay />
+                        <ProductDisplay /> */}
+                
 
 
 
-        </article>
+                </article>
+
+                <div className='grid grid-cols-3 '>
+                    
+                    
+                         <LeftArrow />
+                         <div className='flex justify-around'>
+                            <p>1</p>
+                            <p>2</p>
+                            <p>3</p>
+                         </div>
+                        
+                        <RightArrow />
+                    
+                    
+                       
+                     
+                        
+
+                </div>
             </div>
 
         </div>
