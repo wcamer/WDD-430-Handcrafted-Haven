f5eba384d2415a3852553451e5f9151b08916f99
wcamer <xeus.xiii@hotmail.com>
2024-06-28 16:52:02 -0600

    added commented code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from './ui/header';
 import Footer from './ui/footer';
 import '@/app/ui/global.css';
+// import ProductListing from './ui/components/home-product-listing';
 
 export default function RootLayout({
   children,
@@ -12,6 +13,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        {/* <ProductListing /> */}
         <Footer />
       </body>
     </html>
