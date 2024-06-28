ce5cf29cbc83436d68dec33451737291525a4145
wcamer <xeus.xiii@hotmail.com>
2024-06-27 18:56:38 -0600

    added ProductListing component that will hold all displayed products based on the filtering criterion

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import ProductListing from "/app/ui/components/home-product-listing";
+
 export default function Page() {
   return (
     <main className="h-[32rem]">
       <h1>Home Page</h1>
-      <p className="mt-4	">
+      {/* <p className="mt-4	">
         The space here is to show that the footer stays at the bottom of the
         page.
-      </p>
+      </p> */}
+      <ProductListing  />
     </main>
   );
 }
