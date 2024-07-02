1dda0948e2671c33f771342cb368704efcd5c9de
wcamer <xeus.xiii@hotmail.com>
2024-07-02 02:05:00 -0600

    added styling class to filter buttons container to be targeted by styling to fix alignment

--- app/ui/components/product-filter.tsx
+++ app/ui/components/product-filter.tsx
@@ -5,7 +5,7 @@ export async function ProductFilterButtons(){
     return (
         <div>
             <h2 className="text-center p-6">Sort By:</h2>
-            <div className="flex justify-around p-3 lg:grid grid-cols-6" >
+            <div className={`${Products.filterBtnContainer}`} >
                 <button className={`${Products.filterBtn}`} >A-Z</button>
                 <button className={`${Products.filterBtn}`}>Price</button>
                 <button className={`${Products.filterBtn}`}>New</button>
