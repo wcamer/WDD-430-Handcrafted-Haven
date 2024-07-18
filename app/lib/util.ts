b208c75851f64fc66284a4e7a829820b91cb2eff
wcamer <xeus.xiii@hotmail.com>
2024-07-18 17:20:43 -0600

    functions update

--- app/lib/util.ts
+++ app/lib/util.ts
@@ -1,5 +1,9 @@
+'use server'
 // const {sql, db} = require('@vercel/postgres')
 import {sql} from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { useRouter } from 'next/router'
 
 
 
@@ -57,15 +61,104 @@ export async function fetchByMostRecent(){
 
 // works
 
-export async function fetchUser(id: any): Promise<any>{
+export async function fetchProfile(id: any): Promise<any>{
+
   try{
-    const user = await sql `SELECT * FROM users WHERE user_id=${id}`
-    console.log('here is user in fetchUser...',user.rows[0])
+    //note in the user variable "seller-id" is hardcoded
+    const user = await sql `SELECT * FROM sellers WHERE seller_id=${id}`
+    console.log('here is user in fetchProfile...',user.rows[0])
     return user.rows[0] 
   }catch (error){
-    console.log('fetchUser error... : ', error)
+    console.log('fetchProfile error... : ', error)
+    
+    return '0'
+  }
+  
+}
+
+export async function checkSeller(id: any) {
+  try{
+      let seller = await sql `SELECT * FROM sellers WHERE seller_id=${id}`
+      console.log('@@@@@@@',seller.rows)
+      return seller.rows
+  }catch{
+    console.log(' the checkSeller search didnt work..........')
+  }
+
+}
+
+//give 1 product_id to start the math on all ratings 
+export async function fetchSellersOverallRating(id: any): Promise<any>{
+  let allProductReviews = await sql `Select * FROM reviews WHERE product_id=${id}`
+  let ratingDenom = allProductReviews.rows.length
+  let aggRating = 0
+
+      {
+        for(let i = 0; i < allProductReviews.rows.length; i++){
+            aggRating += allProductReviews.rows[i].review_rating
+        }
+      }
+
+  // console.log('here are the figures for fetchSellersOvearllRating...\n',
+  //   'aggrating: ', aggRating,
+  //   '\n ratingDenom', ratingDenom,
+  //   '\n average score...', (aggRating/ratingDenom)
+  // )
+
+  return aggRating / ratingDenom
+}
+
+
+export async function fetchAllProductsBySeller(id: any): Promise<any>{
+   const prods = await sql `SELECT * FROM products WHERE seller_id=${id}`
+  //  console.log('here are your products in fetchallproducts...',prods.rows)
+    return prods.rows
+
+  }
+
+
+//fully operational
+export async function fetchUser(id: any): Promise<any>{
+  try{
+    console.log('here is id in fetch userrr', id)
+    const user = await sql `SELECT * FROM users WHERE user_id=${id};`
+   
+    // const sellers = await sql `SELECT * FROM sellers;`
+    // const prods = await sql `SELECT * FROM products WHERE product_id=${id};`
+
+    console.log('here is user in fetchUser...',user.rows[0]
+      // ,'here are all the sellers', sellers.rows
+      // ,'\nhere are all prods with the id', prods.rows
+    )
+    return user.rows[0] 
+  }catch (error){
+    console.log('fetchUser error... Operation Canceled ')//, error)
     // let noUser = {user_id:'0'}
-    return 'No User Logged In'
+    // return 'No User Logged In'
+    return '0'
+  }
+  
+}
+
+export async function fetchSeller(id: any): Promise<any>{
+  try{
+    console.log('here is id in fetch userrr', id)
+    const user = await sql `SELECT * FROM sellers WHERE seller_id=${id};`
+   
+    // const sellers = await sql `SELECT * FROM sellers;`
+    // const prods = await sql `SELECT * FROM products WHERE product_id=${id};`
+
+    console.log('here is user in fetchSeller...',user.rows[0]
+      // ,'here are all the sellers', sellers.rows
+      // ,'\nhere are all prods with the id', prods.rows
+    )
+
+    return user.rows[0] 
+  }catch (error){
+    console.log('fetchSeller error... Operation Canceled ')//, error)
+    // let noUser = {user_id:'0'}
+    // return 'No User Logged In'
+    return '0'
   }
   
 }
@@ -82,34 +175,53 @@ export async function fetchProducts(){
 }
 
 export async function fetchProduct(id: any): Promise<any>{
-
+  console.log('herei the id in fetch product ', id)
   // remove the line below it is for testing only
   // await addAnotherReview(id)
   try{
+      //get product by id
       let result = await sql `SELECT * FROM products WHERE product_id=${id}`
-      let sellerName = await sql `SELECT * FROM sellers WHERE seller_id=${result.rows[0].seller_id}`
-      // let productAvgRating = await sql `Select * FROM reviews WHERE product_id=${id}`
-      let allProductReviews = await sql `Select * FROM reviews WHERE product_id=${id}`
-      let ratingDenom = allProductReviews.rows.length
-      let aggRating = 0
-
-      {let productInfo = result.rows[0]
-      for(let i = 0; i < allProductReviews.rows.length; i++){
-          aggRating += allProductReviews.rows[i].review_rating
-      }
-      
+      let productInfo = result.rows[0]
+      console.log('here i produt info in fetch prodcutt;lakj;dlk', productInfo)
+      // get & give seller name
+      let sellerName = await sql `SELECT * FROM sellers WHERE seller_id=${result.rows[0].seller_id};`
       productInfo.sellerName = sellerName.rows[0].seller_name
-      productInfo.product_rating = (aggRating / ratingDenom).toString()
-      // productInfo.product_rating = productAvgRating
-      console.log('here is productInfo.....', productInfo,
-        // '\nhere is the products average rating...', (aggRating/ ratingDenom).toString(),
-        '\n here is allProductReviews...', allProductReviews.rows
-        // '63 out of 70 should be an average of 9...', (aggRating/ratingDenom).toString()
-        // ,'\n here is prodcutAveRating....',productAvgRating.rows[0].review_rating,
-        // '\n check the length of the returned results...', productAvgRating.rows.length,
-        // '\n now i am going to try to do some math...', productAvgRating.rows.length * -10 + productAvgRating.rows[0].review_rating  
-      )
-      return productInfo}
+
+      // get all reviews and find the average for 1 product
+      let allProductReviews = await sql `Select * FROM reviews WHERE product_id=${id}`
+      if(allProductReviews.rows.length < 1){
+        productInfo.product_rating = 0
+        return productInfo
+      }
+      else{
+        let ratingDenom = allProductReviews.rows.length
+        let aggRating = 0
+         // let productInfo = result.rows[0]
+        for(let i = 0; i < allProductReviews.rows.length; i++){
+            aggRating += allProductReviews.rows[i].review_rating
+        }
+        productInfo.product_rating = (aggRating / ratingDenom).toString()
+
+
+          
+          //   // let productInfo = result.rows[0]
+          // for(let i = 0; i < allProductReviews.rows.length; i++){
+          //     aggRating += allProductReviews.rows[i].review_rating
+          // }
+          
+          // productInfo.sellerName = sellerName.rows[0].seller_name
+          // productInfo.product_rating = (aggRating / ratingDenom).toString()
+          // productInfo.product_rating = productAvgRating
+          console.log('here is productInfo.....', productInfo,
+            // '\nhere is the products average rating...', (aggRating/ ratingDenom).toString(),
+            '\n here is allProductReviews...', allProductReviews.rows
+            // '63 out of 70 should be an average of 9...', (aggRating/ratingDenom).toString()
+            // ,'\n here is prodcutAveRating....',productAvgRating.rows[0].review_rating,
+            // '\n check the length of the returned results...', productAvgRating.rows.length,
+            // '\n now i am going to try to do some math...', productAvgRating.rows.length * -10 + productAvgRating.rows[0].review_rating  
+            )
+          return productInfo
+      }
   }catch (error){
     console.log('fetchProduct error: ', error)
     return '0'
@@ -118,19 +230,20 @@ export async function fetchProduct(id: any): Promise<any>{
 
 }
 
+//fully functional
 export  async function setUserProdRating(
   userId: any, 
   productId: any, 
   reviewRating: any, 
   reviewDesc: any){
 
-  let date = new Date()
+  let date =  new Date()
   
   //review id assuming will be auto-gen'ed
   const user = userId;
   const product = productId;
   const review = reviewDesc;
-  const rating = reviewRating;
+  const rating = Number(reviewRating);
   const currentDate = date.toISOString();
 
   console.log('here is...', date,
@@ -142,17 +255,185 @@ export  async function setUserProdRating(
   )
   //will need to send this to an api route???
   try{
-    await sql `INSERT INTO reviews 
-    VALUES (${user}, ${product}, ${review}, ${rating}, ${currentDate});`
+    // const reviews = await sql `SELECT * FROM reviews ;`
+    // console.log('here is reviewsssssssssssss',reviews.rows
+      // , 'here is productttttttttt...', product
+    // )
+
+    // const info = await sql`SELECT 
+    //                     *
+    //                 FROM 
+    //                     information_schema.columns
+    //                 WHERE 
+    //                     table_name = 'reviews';
+    //       `
+    // console.log('here is infoooooooooooooooooooooooooo',info.rows)
+    await sql `INSERT INTO reviews (
+                product_id, user_id, review_rating, date_added, review_description 
+                )
+
+                VALUES (
+                    ${product}, ${user}, ${rating}, ${currentDate}, ${review} 
+        
+                    );`
+
+    const reviews = await sql `select * from reviews where product_id=${product};`
+    console.log('here is reviewsssssssssssss',reviews.rows)
   }catch (error){
     console.log('here is the error in setUserProdRating...',error)
   }
+
+
+  revalidatePath(`/product/${product}`)
   
 
   
 
 
 }
+
+export async function deleteProduct(
+  product_id: any,
+  seller_id: any,
+
+){
+
+  try{
+    const product = await sql `SELECT * FROM products 
+                                WHERE product_id=${product_id} AND seller_id=${seller_id};`
+    console.log('FROM DELELTE PRODUCT...:\nHere is the product in the product table...',product.rows)
+    
+    // await sql`DELETE FROM products 
+    //           WHERE product_id=${product_id} AND seller_id=${seller_id};`
+
+    const products = await sql`SELECT * FROM products;`
+    console.log('from DELETEPRODUCT...:\nhere are all the products...',products.rows)
+
+  }catch (error){
+    console.log('There was an error in deleteProduct.  Operation Canceled!')
+
+    redirect('/product/delete/not-found')
+  }
+
+  // redirect(`/profile/${seller_id}`)
+  redirect(`/product/${product_id}/delete/completed`)
+
+}
+
+//fully functional
+export async function createNewProdcut(
+  sellerId: any,
+  name: any,
+  description: any,
+  price: any,
+  image: any,
+
+){
+
+
+  const initialRating = 0
+  const thumbnail = image
+  const date = new Date()
+  const currentDate = date.toISOString();
+ 
+ //// comment out below
+  console.log(`here are the variablesfor createNewProduct.........\n
+${sellerId}, ${name}, ${description}, ${initialRating} ${Math.round(price)}, ${thumbnail}, ${image}, ${currentDate})
+`
+  )
+  //////
+
+  try{
+    await sql`INSERT INTO products 
+                (seller_id, product_name, product_description, product_rating, 
+                product_price, product_thumbnail, product_image, date_added)
+              VALUES(
+              ${sellerId}, ${name}, ${description}, ${initialRating}, ${Math.round(price)}, ${thumbnail}, ${image}, ${currentDate}
+              
+              );`
+
+  }catch (error){
+
+    console.log("Error in createNewProduct.  Operation canceled!!!", error)//, error)
+
+  }
+
+  //test to see that product was created successfully
+  let id
+  try{
+   const prods = await sql `SELECT * FROM products WHERE seller_id=${sellerId}`
+    console.log('here are prods after it was created...',prods.rows)
+    const newestProductRowIndex = (prods.rows.length) - 1
+    console.log('here is the lengthe of prods.rows.length...', prods.rows.length
+      , newestProductRowIndex
+      , prods.rows[newestProductRowIndex].product_id
+    )
+    id = prods.rows[newestProductRowIndex].product_id
+    console.log('here is the proID for the redirect', id)
+    // revalidatePath('/product/add')
+       // redirect(`/product/${id}`)
+
+
+    // redirect(`/product/${prods.rows[newestProductRowIndex].product_id}`)
+    // redirect(`/product/${id}`)
+
+  }catch (error){
+    console.log('Error trying to retrieve all prods by seller id in createNewProduct...',error)
+  }
+
+
+
+  revalidatePath('/product/add')
+  redirect(`/product/${id}`)
+
+  
+
+
+}
+
+// fully operational
+export async function editProduct(
+  id: any,
+  name: any,
+  description: any,
+  price: any,
+  image: any,
+
+
+){
+  
+  // // console.log('here is the product in editProduct...',product)
+  // console.log('here are the arguments in editProduct...\n',
+  //   'id:...',id,
+  //   '\nname...', name,
+  //   '\ndesc...', description,
+  //   '\nprice...', price,
+  //   '\nimage...', image
+  // )
+
+  try{
+    await sql`UPDATE products 
+              SET 
+                product_name=${name},
+                product_description=${description},
+                product_price=${price},
+                product_image=${image}
+                
+              WHERE
+                product_id=${id};
+                `
+  
+
+  }catch (error){
+    console.log('editProduct error.  Update operation canceled',error)
+    return
+  }
+
+  revalidatePath(`/product/${id}/edit`)
+  redirect(`/product/${id}`)
+  // redirect('/')
+}
+
 // testing only
 async function addAnotherReview(id:any){
   let date = new Date()
