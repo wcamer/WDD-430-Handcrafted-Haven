2a317c827db16077eaf9502bfb84b5514810c561
wcamer <xeus.xiii@hotmail.com>
2024-07-19 17:53:15 -0600

    Added condition if rating for item is 0 and deleted some commented out code

--- app/ui/components/productHighlight.tsx
+++ app/ui/components/productHighlight.tsx
@@ -33,28 +33,6 @@ interface User{
 
 export default async function ProductHighlight({product, user}: {product: any, user: any}) {
     
-    //   const handicraftProducts = [
-    //     {
-    //       name: 'Handwoven Basket',
-    //       imageUrl: '/images/handcraft1.png',
-    //       price: 25.99,
-    //       rating: 4.7,
-    //       id: 'hc001',
-    //       sellerId: 'seller001',
-    //       sellerName: 'CraftsByEmma',
-    //       imageAlt: 'A beautifully handwoven basket with intricate patterns',
-    //       link: 'https://example.com/products/handwoven-basket',
-    //     },]
-
-        // const sellerName = handicraftProducts[0].sellerName // need to run a query or query for the object then break it down
-        // const price = handicraftProducts[0].price
-        // const rating = handicraftProducts[0].rating
-        // const imageUrl = handicraftProducts[0].imageUrl
-        // const imageAlt = handicraftProducts[0].imageAlt
-
-        // const prods = await fetchProducts()
-
-        //const prod = await fetchProduct('84cc3f32-2f63-4d85-ae57-cca720c7c16c')
 
         const prod = product
 
@@ -63,10 +41,13 @@ export default async function ProductHighlight({product, user}: {product: any, u
         const prodName = prod.product_name
         const sellerName = prod.sellerName // need to run a query or query for the object then break it down
         const price = prod.product_price
-        const rating = prod.product_rating
+        let rating = prod.product_rating
         const imageUrl = prod.product_image
         const imageAlt = `This is an image of ${prodName} by ${prod.sellerName}`
 
+        if(rating === 0){
+            rating = 'Pending'
+        }
         console.log('here is pName, sName, rating, and imageurl...', prodName, sellerName, rating, imageUrl)
 
         const [reviewRating, setReviewRating] = useState('')
@@ -112,52 +93,7 @@ export default async function ProductHighlight({product, user}: {product: any, u
             </section>
 
 
-            {/* <section className={`${Highlight.rating} outline md: col-start-2 row-start-2`}>
-                <form className={`${Highlight.ratingForm}`}>
-                    <fieldset className="flex flex-col justify-center align-center items-center">
-                        <legend className="text-2xl text-center">Leave A Review</legend><br></br>
-                        <label className="text-xl" htmlFor='ratingMenu'>Rating Out Of 10</label><br></br>
-                        <select 
-                            className="text-center" 
-                            id='ratingMenu'
-                            name='ratingMenu'
-                            value={reviewRating}
-                            onChange={(e) => setReviewRating(e.target.value)}
-                        
-                        >
-                            <option value="" selected disabled>Pick a rating for the product</option>
-                            <option value="1">1 Worst</option>
-                            <option value="2">2</option> 
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10 Best</option>
-                        </select><br></br>
-                        <label htmlFor='commentSection'>Comment Section</label>
-                        <textarea 
-                            id='commentSection'
-                            name='commentSection'
-                            className={`${Highlight.ratingDesc}`}
-                            placeholder="Leave a comment"
-                            onChange={(e) => setReviewDesc(e.target.value)} 
-                        ></textarea> <br></br>
-                        <button className={`${Highlight.submitBtn} outline p-2 m-2`} type='submit'
-                        // onClick={setUserProdRating('someuser','prodid','reviewer rating', 'review description')}
-                        // onClick={document.querySelector('.Highlight.submitBtn').setAttribute='disabled'}
-                        onClick={handleSubmit}
-                        >
-                            Submit Rating
-                        </button> 
-
-                    </fieldset>
-                    
-                </form>
-            </section> */}
-
+          
             
 
 
